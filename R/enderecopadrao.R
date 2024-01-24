d143007a14bc8192c3f3ffd2018569e250cae0e1
Daniel Herszenhut <dhersz@gmail.com>
2024-01-24 15:18:54 -0300

    corrigindo typo na descricao

--- R/enderecopadrao.R
+++ R/enderecopadrao.R
@@ -1,13 +1,3 @@
-#' enderecopadrao: Padronizador de Endereços Brasileiros (Brazilian Addresses
-#' Standardizer)
-#'
-#' Padroniza endereços brasileiros a partir de diferentes critérios. Os métodos
-#' de padronização incluem apenas manipulações básicas de strings, não
-#' oferecendo suporte a correspondências probabilísticas entre strings.
-#' (Standardadizes brazilian addresses using different criteria. Standardization
-#' methods include only basic string manipulation, not supporting probabilistic
-#' matches between strings.)
-#'
 #' @docType package
 #' @name enderecopadrao
 #' @aliases enderecopadrao-package pacote-enderecopadrao
