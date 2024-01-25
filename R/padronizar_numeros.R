48f91b67915097ced175e5f2bf5074a37b8d9519
Daniel Herszenhut <dhersz@gmail.com>
2024-01-25 13:17:12 -0300

    primeira versao da padronizar_numeros()

---
+++ R/padronizar_numeros.R
@@ -0,0 +1,63 @@
+#' Padronizar números de logradouros
+#'
+#' Padroniza um vetor de números de logradouros. Veja a seção *Detalhes* para
+#' mais informações sobre a padronização.
+#'
+#' @param numeros Um vetor de caracteres ou números. Os números de logradouro a
+#'   serem padronizados.
+#'
+#' @return Um vetor de caracteres com os números de logradouros padronizados.
+#'
+#' @section Detalhes:
+#' Operações realizadas durante a padronização:
+#'
+#' 1. conversão para caracter, se o input for numérico;
+#' 2. remoção de espaços em branco antes e depois dos números e de espaços em
+#' branco em excesso entre números;
+#' 3. remoção de zeros à esquerda;
+#' 4. substituição de números vazios e de variações de SN (SN, S N, S.N., S./N.,
+#' etc) por S/N.
+#'
+#' @examples
+#' numeros <- c("0210", "001", "1", "", "S N", "S/N", "SN", "0180  0181")
+#' padronizar_numeros(numeros)
+#'
+#' numeros <- c(210, 1, 10000, NA)
+#' padronizar_numeros(numeros)
+#'
+#' @export
+padronizar_numeros <- function(numeros) {
+  checkmate::assert(
+    checkmate::check_character(numeros),
+    checkmate::check_numeric(numeros),
+    combine = "or"
+  )
+
+  if (is.numeric(numeros)) {
+    numeros_na <- which(is.na(numeros))
+
+    numeros_padrao <- formatC(numeros, format = "d")
+    numeros_padrao[numeros_na] <- "S/N"
+
+    return(numeros_padrao)
+  }
+
+  # alguns numeros podem vir vazios ou como NAs, fazendo com que as operacoes
+  # abaixo nao convertam seus valores adequadamente. nesses casos, identificamos
+  # seus indices para manualmente imputar "S/N" ao final
+
+  indice_num_vazio <- which(numeros == "" | is.na(numeros))
+
+  numeros_padrao <- stringr::str_squish(numeros)
+  numeros_padrao <- stringr::str_replace_all(
+    numeros_padrao,
+    c(
+      "\\b0+(\\d+)\\b" = "\\1",    # 015 -> 15, 00001 -> 1, 0180 0181 -> 180 181
+      "S\\.?( |\\/)?N\\.?" = "S/N" # SN ou S.N. ou S N ou .... -> S/N
+    )
+  )
+
+  numeros_padrao[indice_num_vazio] <- "S/N"
+
+  return(numeros_padrao)
+}
