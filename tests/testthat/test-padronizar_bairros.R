69700e5c4c9298ede0b4304004cdd5dd853f99b7
Daniel Herszenhut <dhersz@gmail.com>
2024-02-05 11:38:58 -0300

    testezinhos basicos e novas regras na padronizar_bairros()

---
+++ tests/testthat/test-padronizar_bairros.R
@@ -0,0 +1,17 @@
+test_that("da erro com inputs != de caracteres", {
+  expect_error(padronizar_bairros(12))
+})
+
+test_that("padroniza corretamente", {
+  # complicado fazer um teste pra cada uma das regexs usadas. testando só um
+  # basiquinho da manipulação, depois pensamos melhor se vale a pena fazer um
+  # teste pra cada regex ou não
+
+  expect_equal(padronizar_bairros("JARDIM  BOTÂNICO"), "JARDIM BOTANICO")
+  expect_equal(padronizar_bairros("jardim botanico"), "JARDIM BOTANICO")
+  expect_equal(padronizar_bairros("jd..botanico"), "JARDIM BOTANICO")
+})
+
+test_that("lida com vetores vazios corretamente", {
+  expect_equal(padronizar_bairros(character(0)), character(0))
+})
